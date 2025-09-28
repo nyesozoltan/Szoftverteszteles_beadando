9a4e93dfbb259e34dc8ba56f47aaaefe1533c384
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-09-28 10:44:11 +0200

    1. ZÖLD: Kódalakítás, tesztparametrizálás

--- src/test/java/szoftteszt/MoonRoverTest.java
+++ src/test/java/szoftteszt/MoonRoverTest.java
@@ -1,15 +1,27 @@
 package  szoftteszt;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
 public class MoonRoverTest {
 
-    @Test 
-    void rotate_right() {
-        MoonRover moonrover = new MoonRover();
-        assertThat(moonrover.execute("R"), is("0:0:E"));       
+    private MoonRover moonrover;
+
+    @BeforeEach
+    public void initialise() {
+        moonrover = new MoonRover();
     }
 
+   @ParameterizedTest
+    @CsvSource({
+        "R, 0:0:E"
+    }) 
+
+    public void
+    rotate_right(String commands, String position) {
+         assertThat(moonrover.execute(commands), is(position));              
+    }
 }
---
+++ src/test/java/szoftteszt/RunWith.java
@@ -0,0 +1,5 @@
+package szoftteszt;
+
+public @interface RunWith {
+
+}
