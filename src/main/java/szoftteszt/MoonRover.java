befe3265d64b4bee7e435838cb2b1adb2a88e157
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-09-28 10:16:57 +0200

    1. PIROS: Holdjáró "R" fordul bemenetre "0:0:E" outputot várok.

---
+++ src/main/java/szoftteszt/MoonRover.java
@@ -0,0 +1,8 @@
+package szoftteszt;
+
+class MoonRover {
+
+    String execute(String commands) {
+       throw new UnsupportedOperationException("Not implemented yet");       
+    }
+}
---
+++ src/test/java/szoftteszt/MoonRoverTest.java
@@ -0,0 +1,14 @@
+package  szoftteszt;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MoonRoverTest {
+
+    @Test 
+    void rotate_right() {
+        MoonRover moonrover = new MoonRover();
+        assertThat(moonrover.execute("R"), is("0:0:E"));       
+    }
+}
