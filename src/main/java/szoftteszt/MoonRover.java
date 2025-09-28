8dda7ee15cac028644aa2cbff8b242d335c70536
Nyeso Zoltan <zoltan.nyeso@student.uni-miskolc.hu>
2025-09-28 11:40:18 +0200

    3. ZÖLD:  RRR inputra elvárt 0:0:W

--- src/main/java/szoftteszt/MoonRover.java
+++ src/main/java/szoftteszt/MoonRover.java
@@ -13,9 +13,11 @@ class MoonRover {
 
     private String rotateRight() {
         if (direction == "N") {
-            return "E";             
-        } else {
-            return "S";              
+            return "E";
+        } else if (direction == "E") {
+            return "S";
+        } else  {
+            return "W";                   
         }        
     }
 }
